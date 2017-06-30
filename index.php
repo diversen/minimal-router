e0546461f3a9a932da35a043ec5857daa48b4270
Dennis Iversen <dennis.iversen@gmail.com>
2017-06-30 19:21:24 +0200

    auto commit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include_once "test.php";
 // You will not need the above includes
 // if you use have added class to vendor
 // using composer
-$r = new \diversen\router();
+$r = new \minimal\router();
 
 // Example 'test' route. Match e.g. /test/123123 (or any int as last part of path)
 $route =  array (
--- router.php
+++ router.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace diversen;
+namespace minimal;
 
 /**
  * Very simple router class
